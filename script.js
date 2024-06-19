547d79d1254fcbf9c9d537a1cfa80856bb48f8ed
Mathew Gutoskie <98121688+mathewgut@users.noreply.github.com>
2024-06-19 16:22:19 -0400

    created getHumanChoice function

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+humanScore = 0;
+computerScore = 0;
+
+
 function getComputerChoice () {
     // multiply by three for the three different choices
     // you can also do 0-2 but my brain likes 1-3
-    choice = Math.random()* 3;
+    choice = Math.random() * 3;
     console.log("Unrounded value: " + choice);
     // use celiing so that the number cannot be 0
     choice = Math.ceil(choice);
@@ -18,3 +22,29 @@ function getComputerChoice () {
     }
 }
 console.log(getComputerChoice());
+
+function getHumanChoice () {
+// Initialize prompt and convert to lowercase for easier comparing 
+  userPrompt = prompt("Rock, paper, scissors!");
+  userChoice = userPrompt.toLowerCase();
+
+  // return 0 if not a valid answer
+  switch(userChoice){
+    case "rock":
+        return "rock";
+    case "scissors":
+        return "scissors";
+    case "paper":
+        return "paper";
+    default:
+        return 0;
+  }
+}
+
+/*
+
+repeat until we get a valid answer
+while (getHumanChoice() === 0)
+    getHumanChoice()
+*/
+
